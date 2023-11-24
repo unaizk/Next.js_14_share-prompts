f506b85fb02301fd97b782c4b867382d3273022f
root <root@Unaislaptop>
2023-11-24 19:10:17 +0530

    just styled h and p tag in home page

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='w-full flex-center flex-col'>
+        <h1 className='head_text text-center'>
+            Discover & Share
+            <br/>
+            <span className='orange_gradient text-center'>AI-Powered Prompts</span>
+        </h1>
+        
+        <p className='desc text-center'>
+            Promptia is an open-source AI prompting tool for modern world to dicover, create and share creative prompts.
+        </p>
+       
+        
+    </section>
+    
   )
 }
 
