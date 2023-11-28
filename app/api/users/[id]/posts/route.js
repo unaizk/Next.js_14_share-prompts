fb76301b349713d8fcfa534d69f3823544791015
root <root@Unaislaptop>
2023-11-28 16:37:53 +0530

    added a dymanic route and fetch the posts that created by specific user

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,18 @@
+import Prompt from "@models/promptModel";
+import { connectToDB } from "@utils/database";
+
+
+export const GET = async(req,res,{params}) =>{
+    try {
+        await connectToDB();
+        const prompts = await Prompt.find({creator : params.id}).populate('creator')
+
+        return new Response(JSON.stringify(prompts),{
+            status : 200
+        })
+    } catch (error) {
+        return new Response("Failed to fetch all prompts",{
+            status : 500
+        })
+    }
+}
