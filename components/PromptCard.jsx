f6d4555454aedd53bcf6da59300e6104d17fb9bf
root <root@Unaislaptop>
2023-11-28 16:17:00 +0530

    i Just added the copy button and when it clicks the prompts will copy into clipboard

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,6 +5,12 @@ import { usePathname, useRouter } from 'next/navigation'
 
 const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
   const [copied,setCopied] = useState('')
+
+  const handleCopy = async()=>{
+    setCopied(post.prompt)
+    navigator.clipboard.writeText(post.prompt)
+    setTimeout(() => setCopied(''),3000)
+  }
   return (
     <div className='prompt_card'>
       <div className='flex justify-between item-start gap-5'>
@@ -15,7 +21,7 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
                 <p className='font-inter text-sm text-gray-500'>{post.creator.email}</p>
             </div>
           </div>
-          <div className='copy_btn' onClick={() =>{}}>
+          <div className='copy_btn' onClick={handleCopy}>
             <Image src={copied === post.prompt ? '/assets/icons/tick.svg' : '/assets/icons/copy.svg'} width={12} height={12}/>
           </div>
       </div>
